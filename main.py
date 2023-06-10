6cd733f89b5bbd02763f2c2e80c4221a3e9e4198
ajinkyagorad <ajinkyagorad@gmail.com>
2023-06-10 20:59:26 +0300

    progressed

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from PyQt5.QtCore import Qt
 import sys
 import os
 import glob
-
-
+import shutil
+import subprocess
+import json
 
 class MainApp(QApplication):
     def __init__(self, sys_argv):
@@ -29,7 +30,25 @@ class MainApp(QApplication):
         self.device_spinner = QComboBox()
         self.device_spinner.addItems(['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
         parameters_layout.addWidget(self.device_spinner)
+        
+   
 
+
+        # Working Directory Selector
+        self.working_dir_edit = QLineEdit()
+        parameters_layout.addWidget(QLabel("Working Directory:"))
+        parameters_layout.addWidget(self.working_dir_edit)
+        self.select_working_dir_button = QPushButton("Select Working Directory")
+        self.select_working_dir_button.clicked.connect(self.select_working_dir)
+        parameters_layout.addWidget(self.select_working_dir_button)
+        
+        # Set the default working directory
+        self.working_dir = "/home/triton/Desktop/SeaIce/data/journal-rgb-dataset/SemantixTemp"
+        self.working_dir_edit.setText(self.working_dir)
+        # Check if the directory exists and create it if necessary
+        os.makedirs(self.working_dir, exist_ok=True)
+        
+        
         # Buttons
         button_layout = QHBoxLayout()
         self.layout.addLayout(button_layout)
@@ -60,19 +79,59 @@ class MainApp(QApplication):
         self.window.setLayout(self.layout)
         self.window.show()
 
+    def select_working_dir(self):
+        self.working_dir = QFileDialog.getExistingDirectory(self.window, "Select Working Directory")
+        self.working_dir_edit.setText(self.working_dir)
+
+
     def load_images_labels(self):
         options = QFileDialog.Options()
         file_dialog = QFileDialog()
-        filenames, _ = file_dialog.getOpenFileNames(filter="Images (*.jpg *.png)", options=options)
+        filenames, _ = file_dialog.getOpenFileNames(self.window, filter="Images (*.jpg *.png)", options=options)
 
         valid_files = [f for f in filenames if os.path.isfile(f.rsplit('.', 1)[0] + '.json')]
 
         self.results_console.appendPlainText(f"{len(valid_files)} files loaded, {len(filenames) - len(valid_files)} json files missing\n")
 
+        imglabels_dir = os.path.join(self.working_dir, 'imglabels')
+        os.makedirs(imglabels_dir, exist_ok=True)
+
+        json_file_paths = []
+
+        for file_path in valid_files:
+            # Copy the image file to the new directory
+            shutil.copy(file_path, imglabels_dir)
+            # Add the path of the corresponding json file
+            json_file = file_path.rsplit('.', 1)[0] + '.json'
+            json_file_paths.append(json_file)
+            # Copy the json file to the new directory
+            shutil.copy(json_file, imglabels_dir)
+
+
+        self.results_console.appendPlainText("Copied files to: " + imglabels_dir)
+
+
 
     def prepare_dataset(self):
-        # Implement logic here
         self.results_console.appendPlainText("Prepare Dataset clicked\n")
+
+        # Directories for input and output
+        input_dir = os.path.join(self.working_dir, 'imglabels')
+        #output_dir = os.path.join(self.working_dir, 'YOLOdataset')
+
+        # Ensure output directory exists
+        #os.makedirs(output_dir, exist_ok=True)
+
+        # Get the path to the label list file
+        label_list_file = os.path.join(self.working_dir, 'labels_file_list.txt')
+
+        # Call labelme2yolo with subprocess
+        subprocess.check_call(["labelme2yolo","--json_dir", input_dir ])
+
+        self.results_console.appendPlainText("Dataset prepared in YOLO format\n")
+
+
+
 
     def train_data(self):
         # Implement logic here
